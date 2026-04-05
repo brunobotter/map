7d4fe357168bb987cce68039054685a53bae1954
brunobotter <62908563+brunobotter@users.noreply.github.com>
2026-04-05 08:59:35 -0300

    fat

--- app/main/providers/repository_provider.go
+++ app/main/providers/repository_provider.go
@@ -1,8 +1,6 @@
 package providers
 
 import (
-	"github.com/brunobotter/map/application/repo"
-	infraRepo "github.com/brunobotter/map/infra/repo"
 	"github.com/brunobotter/map/main/container"
 )
 
@@ -12,7 +10,5 @@ func NewRepositoryProvider() *RepositoryProvider {
 	return &RepositoryProvider{}
 }
 func (p *RepositoryProvider) Register(c container.Container) {
-	c.Singleton(func() repo.MapRepository {
-		return infraRepo.NewMapRepository()
-	})
+
 }
